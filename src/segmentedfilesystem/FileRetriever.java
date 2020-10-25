a67473682a52929110aab24d5df1834a9baa139e
oswoo003 <oswoo003@umn.edu>
2020-10-25 16:45:16 -0500

    Coded FileRetriever, will need to create the other classes still.

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -1,13 +1,21 @@
 package segmentedfilesystem;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class FileRetriever {
+	String server;
+	int port;
 
 	public FileRetriever(String server, int port) {
         // Save the server and port for use in `downloadFiles()`
         //...
+		this.server = server;
+		this.port = port;
 	}
 
-	public void downloadFiles() {
+	public void downloadFiles() throws IOException {
         // Do all the heavy lifting here.
         // This should
         //   * Connect to the server
@@ -20,6 +28,21 @@ public class FileRetriever {
         // PacketManager.allPacketsReceived() that you could
         // call for that, but there are a bunch of possible
         // ways.
-	}
 
+		byte[] buf = new byte[1028];
+
+		DatagramSocket socket = new DatagramSocket(port);
+		DatagramPacket sendPacket = new DatagramPacket(buf, buf.length, port);
+
+		socket.send(sendPacket);
+		// Need to implement PacketManager class
+		PacketManager pm = new PacketManager();
+
+		while (!pm.allPacketsReceived()) {
+			DatagramPacket receivedPacket = new DatagramPacket(buf, buf.length);
+			socket.receive(receivedPacket);
+
+			pm.intake(receivedPacket);
+		}
+	}
 }
